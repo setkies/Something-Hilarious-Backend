13be3ff07ac17d5ced053417431ea8e95fab8404
qlido <jason7530@naver.com>
2023-11-14 12:05:20 +0900

    #5

--- src/main/java/com/setkies/sinp/domain/project/Project.java
+++ src/main/java/com/setkies/sinp/domain/project/Project.java
@@ -1,20 +1,27 @@
 package com.setkies.sinp.domain.project;
 
+import com.setkies.sinp.domain.user.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +37,8 @@ public class Project {
 
     private Long targetFund;
     private LocalDateTime fundEndTime;
+
+    @ManyToOne
+    private User author;
+
 }
